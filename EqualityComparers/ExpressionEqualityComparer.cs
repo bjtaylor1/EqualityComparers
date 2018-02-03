c045a0cc31889dc2aea8271a9c5da6b02a575a9d
Ben Taylor <bentaylortheonly@gmail.com>
2018-02-03 10:37:23 +0000

    initial commit

---
+++ EqualityComparers/ExpressionEqualityComparer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EqualityComparers
+{
+    public class ExpressionEqualityComparer<T> : IEqualityComparer<T>
+    {
+        private readonly Func<T, T, bool> equal;
+        private readonly Func<T, int> getHashCode;
+
+        private ExpressionEqualityComparer()
+        {
+
+        }
+
+        public static IEqualityComparer<T> Create(Func<T,T,bool> equal)
+        {
+            return new ExpressionEqualityComparer<T>(equal, t => t.GetHashCode());
+        }
+
+        public static IEqualityComparer<T> Create(Func<T, T, bool> equal, Func<T,int> getHashCode)
+        {
+            return new ExpressionEqualityComparer<T>(equal, getHashCode);
+        }
+
+        private ExpressionEqualityComparer(Func<T, T, bool> equal, Func<T,int> getHashCode)
+        {
+            this.equal = equal;
+            this.getHashCode = getHashCode;
+        }
+
+        public bool Equals(T x, T y)
+        {
+            return equal(x, y);
+        }
+
+        public int GetHashCode(T obj)
+        {
+            return getHashCode(obj);
+        }
+    }
+}
